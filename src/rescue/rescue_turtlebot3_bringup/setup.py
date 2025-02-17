14d38e15733779c8e4bc97833a9f2a54cac65a62
jhj1111 <happyijun@naver.com>
2025-02-17 21:59:07 +0900

    패키지 기본 구조, turtlebot3, drone spawn

---
+++ src/rescue/rescue_turtlebot3_bringup/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import os, glob
+
+package_name = 'rescue_turtlebot3_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, "launch"), glob.glob('launch/*launch.[pxy][yma]*')),
+        (os.path.join('share', package_name, "rviz"), glob.glob('rviz/*.rviz')),
+        (os.path.join('share', package_name, "map"), glob.glob('map/*')),
+        #(os.path.join('share', package_name, "waypoints"), glob('waypoints/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jhj',
+    maintainer_email='happyijun@naver.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
