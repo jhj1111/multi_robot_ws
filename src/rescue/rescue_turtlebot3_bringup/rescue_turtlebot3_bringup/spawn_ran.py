3980f8d91c4fd9b3ac772ed63170c31bf1919d8f
HYUJUNKIM <hyujun9260@gmail.com>
2025-02-18 18:24:05 +0900

    spawn_box
    spawn_box

---
+++ src/rescue/rescue_turtlebot3_bringup/rescue_turtlebot3_bringup/spawn_ran.py
@@ -0,0 +1,54 @@
+import os
+import rclpy
+import random
+from gazebo_msgs.srv import SpawnEntity
+
+def main():
+    # Start node
+    sdf_pose_x = random.uniform(1.0, 3.0)
+    sdf_pose_y = random.uniform(1.0, 3.0)
+    sdf_pose_z = 0.1
+
+    rclpy.init()
+    node = rclpy.create_node("entity_spawner")
+
+    node.get_logger().info(
+        'Creating Service client to connect to `/spawn_entity`')
+    client = node.create_client(SpawnEntity, "/spawn_entity")
+
+    node.get_logger().info("Connecting to `/spawn_entity` service...")
+    
+    if not client.service_is_ready():
+        client.wait_for_service()
+        node.get_logger().info("...connected!")
+
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    sdf_file_path = os.path.join(script_dir, "models/model.sdf") 
+
+    # Set data for request
+    request = SpawnEntity.Request()
+    request.name = "red"
+    request.xml = open(sdf_file_path, 'r').read()
+    request.robot_namespace = "red"
+    request.initial_pose.position.x = float(sdf_pose_x)
+    request.initial_pose.position.y = float(sdf_pose_y)
+    request.initial_pose.position.z = float(sdf_pose_z)
+
+    node.get_logger().info("Sending service request to `/spawn_entity`")
+
+    future = client.call_async(request)
+
+    rclpy.spin_until_future_complete(node, future)
+    if future.result() is not None:
+        print('response: %r' % future.result())
+    else:
+        raise RuntimeError(
+            'exception while calling service: %r' % future.exception())
+
+    node.get_logger().info("Done! Shutting down node.")
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
