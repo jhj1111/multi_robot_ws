bf5fa6d638bac2c35c614a970b8945dcde5b70f7
jhj1111 <happyijun@naver.com>
2025-02-18 10:10:21 +0900

    worlds 경로 수정 - turtlebot3 -> rescue_turtlebot3_bringup

--- src/rescue/rescue_turtlebot3_bringup/launch/map_world_nogazebo.launch.py
+++ src/rescue/rescue_turtlebot3_bringup/launch/map_world_nogazebo.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
     y_pose = LaunchConfiguration('y_pose', default='-0.5')
 
     world = os.path.join(
-        get_package_share_directory('turtlebot3_gazebo'),
+        get_package_share_directory('rescue_turtlebot3_bringup'),
         'worlds',
         'map.world'
     )
